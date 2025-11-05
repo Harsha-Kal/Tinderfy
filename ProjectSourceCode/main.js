0715cca7a5c511120a1dc9f3c6a18d2c07971fa4
Amit Wieder <wiederamit@gmail.com>
2025-11-05 16:15:48 -0700

    fleshed out some get and post requests, fill in blanks as html file structure becomes more clear

--- ProjectSourceCode/main.js
+++ ProjectSourceCode/main.js
@@ -7,3 +7,29 @@ const pgp = require('pg-promise')(); // To connect to the Postgres DB from the n
 const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcryptjs');
+
+app.get("/", (req, res) => {
+    //res.render('file');
+});
+app.get("/login", (req, res) => {
+    //res.render('file');
+});
+app.get("/register", (req, res) => {
+    //res.render(file);
+});
+app.post("/register", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
+    try{
+        //const insertedUser = await db.one(query, [username, hash])
+        //console.log(insertedUser);
+        //res.redirect(login page);
+    }
+    catch(err){
+        const error = true;
+        console.log(err);
+        //res.render(register page with error);
+    }
+});
