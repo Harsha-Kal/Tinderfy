158638273ad6968110108c1f37981fa2fe92bcb9
Amit Wieder <wiederamit@gmail.com>
2025-11-06 11:35:09 -0700

    deleted redundant files to prepare for merging register branch

--- ProjectSourceCode/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); // To build an application server or API
-const app = express(); //creates express instance
-const handlebars = require('express-handlebars'); 
-const Handlebars = require('handlebars'); //imports handlebar library
-const path = require('path');
-const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
-const bodyParser = require('body-parser');
-const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
-const bcrypt = require('bcryptjs');
